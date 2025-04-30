513f96051ebd2155c7a409b7c03fbc53227d0048
klaenex <klaenex.29@gmail.com>
2025-04-30 09:14:18 +0200

    mission

--- wp-content/themes/fcjmp_custom/page-mission.php
+++ wp-content/themes/fcjmp_custom/page-mission.php
@@ -2,7 +2,7 @@
 /*
 Template Name: Mission
 */
-phpinfo();
+
 
 get_header();
 ?>
@@ -17,38 +17,80 @@ get_header();
     </div>
 
 </section>
-<div class="content">
+<section class="section">
 
-    <ul>
-        <li class="list">
-            <h2>Égalité des chances</h2>
-            <p>La FCJMP oeuvre pour une jeunesse qui bénéficie des mêmes opportunités, indépendamment de son origine sociale, économique ou culturelle. Elle défend une approche inclusive et lutte contre les inégalités.</p>
-        </li>
-        <li>
-            <h2>Engagement et participation active des jeunes
-            </h2>
-            <p>Elle encourage l’implication des jeunes dans la société en leur offrant des espaces de réflexion, d’expression et d’action, afin qu’ils deviennent acteurs de leur propre parcours et du changement social.</p>
-        </li>
-        <li>
-            <h2>Éducation populaire et émancipation</h2>
-            <p>La fédération soutient un modèle éducatif basé sur l’apprentissage par l’expérience, la transmission de savoirs critiques et le développement de la citoyenneté active pour permettre aux jeunes de s’émanciper.</p>
-        </li>
-        <li>
-            <h2>Solidarité et justice sociale</h2>
-            <p>La FCJMP promeut des valeurs de partage, d’entraide et de coopération pour renforcer le lien social et construire une société plus équitable et solidaire.</p>
-        </li>
-        <li>
-            <h2>Diversité et pluralisme
-            </h2>
-            <p>Elle défend une approche ouverte et respectueuse des différences, encourageant la rencontre interculturelle et intergénérationnelle, tout en veillant à la diversité des expressions et des opinions.</p>
-        </li>
-        <li>
-            <h2>Innovation et créativité
-            </h2>
-            <p>La FCJMP soutient des approches pédagogiques et socioculturelles innovantes pour répondre aux besoins changeants des jeunes et favoriser leur épanouissement à travers des projets créatifs et participatifs.</p>
-        </li>
-    </ul>
-</div>
+
+    <div class="content">
+
+        <ul class="list">
+            <li class="list list-mission">
+                <div>
+                    <h2>Égalité des chances</h2>
+                    <p>La FCJMP oeuvre pour une jeunesse qui bénéficie des mêmes opportunités, indépendamment de son origine sociale, économique ou culturelle. Elle défend une approche inclusive et lutte contre les inégalités.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_1_mission.png" alt="Icône Email" loading="lazy">
+
+            </li>
+            <li class="list list-mission">
+                <div>
+                    <h2>Engagement et participation active des jeunes
+                    </h2>
+                    <p>Elle encourage l’implication des jeunes dans la société en leur offrant des espaces de réflexion, d’expression et d’action, afin qu’ils deviennent acteurs de leur propre parcours et du changement social.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_2_mission.png" alt="Icône Email" loading="lazy">
+            </li>
+        </ul>
+    </div>
+
+</section>
+<section class="section">
+
+
+    <div class="content content-green">
+        <ul class="list">
+            <li class="list list-mission">
+                <div>
+                    <h2>Éducation populaire et émancipation</h2>
+                    <p>La fédération soutient un modèle éducatif basé sur l’apprentissage par l’expérience, la transmission de savoirs critiques et le développement de la citoyenneté active pour permettre aux jeunes de s’émanciper.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_3_mission.png" alt="Icône Email" loading="lazy">
+            </li>
+            <li class="list list-mission">
+                <div>
+                    <h2>Solidarité et justice sociale</h2>
+                    <p>La FCJMP promeut des valeurs de partage, d’entraide et de coopération pour renforcer le lien social et construire une société plus équitable et solidaire.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_4_mission.png" alt="Icône Email" loading="lazy">
+            </li>
+        </ul>
+    </div>
+</section>
+
+
+
+<section class="section">
+    <div class="content">
+        <ul>
+
+
+            <li class="list list-mission">
+                <div>
+                    <h2>Diversité et pluralisme
+                    </h2>
+                    <p>Elle défend une approche ouverte et respectueuse des différences, encourageant la rencontre interculturelle et intergénérationnelle, tout en veillant à la diversité des expressions et des opinions.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_5_mission.png" alt="Icône Email" loading="lazy">
+            </li>
+            <li class="list list-mission">
+                <div>
+                    <h2>Innovation et créativité
+                    </h2>
+                    <p>La FCJMP soutient des approches pédagogiques et socioculturelles innovantes pour répondre aux besoins changeants des jeunes et favoriser leur épanouissement à travers des projets créatifs et participatifs.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img_1_mission.png" alt="Icône Email" loading="lazy">
+            </li>
+        </ul>
+    </div>
 </section>
 
 <?php
